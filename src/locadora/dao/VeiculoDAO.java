75f93684d027b858fb1d33508fb7b460f42590c8
hellenilda <hellen95limaaraujo@gmail.com>
2025-03-08 12:53:19 -0300

    :sparkles: feat: VeiculoDAO (salvar e carregar) + Gson Lib

--- src/locadora/dao/VeiculoDAO.java
+++ src/locadora/dao/VeiculoDAO.java
@@ -2,16 +2,20 @@ package locadora.dao;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import locadora.model.Veiculo;
 
 public class VeiculoDAO implements Persistencia<Veiculo> {
-	private static final String ARQUIVO = "veiculos.java";
+	private static final String ARQUIVO = "veiculos.json";
+    private static final Gson gson = new Gson();
 
 	public void salvar(List<Veiculo> veiculos) {
 		try (FileWriter writer = new FileWriter(ARQUIVO)) {
-			// A implementar
+			gson.toJson(veiculos, writer);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -19,10 +23,9 @@ public class VeiculoDAO implements Persistencia<Veiculo> {
 	
 	public List<Veiculo> carregar() {
 		try (FileReader reader = new FileReader(ARQUIVO)) {
-			// A implementar
-			return null;
+			return gson.fromJson(reader, new TypeToken<List<Veiculo>>() {}.getType());
 		} catch (Exception e) {
-			return null; // A implementar
+			return new ArrayList<>(); // Em caso de erro, retorna uma lista vazia
 		}
 	}
 }

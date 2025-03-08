5dcacbd7245c3c3f4ac653d939cff04edb15a6b2
hellenilda <hellen95limaaraujo@gmail.com>
2025-03-08 16:33:38 -0300

    :recycle: refactor: remoção da lista veiculos na Interface de Persistência

--- src/locadora/dao/Persistencia.java
+++ src/locadora/dao/Persistencia.java
@@ -1,11 +1,7 @@
 package locadora.dao;
 
-import java.util.List;
-
-import locadora.model.Veiculo;
-
 public interface Persistencia<T> {
 	void salvar(); // T é um tipo genérico. Flexível, para evitar o casting
-	public static final List<Veiculo> veiculos = null;
+
 	void carregar();
 }
